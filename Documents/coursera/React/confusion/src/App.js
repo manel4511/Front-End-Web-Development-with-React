6069ceb734f436c28863df8fd330bb2b02255c19
manel ben salah <manel.bensalah@esprit.tn>
2020-07-25 15:01:26 +0100

    React Redux Form Validation

--- Documents/coursera/React/confusion/src/App.js
+++ Documents/coursera/React/confusion/src/App.js
@@ -3,17 +3,22 @@ import React, { Component } from 'react';
 import Main from './components/MainComponent';
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
 
+const store = ConfigureStore();
 class App extends Component {
 
   render() {  
     return (
         // adding BrowserRouter. the application now is congigured to make use of the react router
+        <Provider store={store}>
         <BrowserRouter>
-            <div className="App">
-                <Main /> 
-            </div>
+          <div className="App">
+            <Main />
+          </div>
         </BrowserRouter>
+      </Provider>
     );
   }
 }
