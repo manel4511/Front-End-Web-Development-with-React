f09f5668750fef4895c9e5e869b2561801e7baa8
manel ben salah <manel.bensalah@esprit.tn>
2020-07-26 11:46:46 +0100

    Fetch from Server

--- Documents/coursera/React/confusion/src/components/DishdetailComponent.js
+++ Documents/coursera/React/confusion/src/components/DishdetailComponent.js
@@ -18,14 +18,14 @@ import {
 } from "reactstrap";
 import { Control, LocalForm, Errors } from "react-redux-form";
 import { Link } from "react-router-dom";
-
+import { baseUrl } from '../shared/baseUrl';
 import { Loading } from './LoadingComponent';
     function RenderDish({dish}) {
         if (dish != null) {
             return (
                 <div className="col-12 col-md-5 m-1">
                     <Card>
-                        <CardImg width="100%" src={dish.image} alt={dish.name} />
+                    <CardImg top src={baseUrl + dish.image} alt={dish.name} />
                         <CardBody>
                             <CardTitle> {dish.name} </CardTitle>
                             <CardText> {dish.description} </CardText>
